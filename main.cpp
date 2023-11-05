38b00c3cd503084d6eacb46a2b6a3058bb78dd78
Matt Christiansen <mattchristiansen59@gmail.com>
2023-11-05 12:41:49 -0800

    added Qt for GUI foundation

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 #include <iostream>
 #include <QApplication>
 #include <QDebug>
+#include <QLabel>
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
     try {
         auto *bear = new Animal(500.0, 15.0, 20.0, "berries", "Bear1", "Roar",
                                   "Brown", "canidae");
@@ -28,6 +29,21 @@ int main() {
         calc->multiply(1, 2);
         cout << "Successfully created an animal and calculator object." << endl;
 
+    } catch (exception &e) {
+        cout << "Error: " << e.what() << endl;
+    }
+
+    // try to create the QApplication
+    try {
+        QApplication app(argc, argv);
+        QLabel label("Hello Qt!  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
+                     "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation "
+                     "ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in "
+                     "reprehenderit in ""voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur "
+                     "sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est "
+                     "laborum");
+        label.show();
+        return QApplication::exec();
     } catch (exception &e) {
         cout << "Error: " << e.what() << endl;
     }
---
+++ qt_test.pro
@@ -0,0 +1,9 @@
+QT       += core gui
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = qt_test
+CONFIG   += console
+CONFIG   -= app_bundle
+TEMPLATE = app
+
+SOURCES += main.cpp
