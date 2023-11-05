779ef825222a97970444f3e49933fdc09563535b
Matt Christiansen <mattchristiansen59@gmail.com>
2023-11-05 12:59:03 -0800

    cleaned up GUI

--- main.cpp
+++ main.cpp
@@ -4,49 +4,49 @@
 #include <QApplication>
 #include <QDebug>
 #include <QLabel>
+#include <QString>
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    QApplication app(argc, argv); // Move QApplication up so it is initialized first
+
     try {
         auto *bear = new Animal(500.0, 15.0, 20.0, "berries", "Bear1", "Roar",
-                                  "Brown", "canidae");
+                                "Brown", "canidae");
         auto *calc = new Calculator();
 
-        cout << "Bear details: " << endl;
-        cout << "Weight: " << bear->getWeight() << endl;
-        cout << "Height: " << bear->getHeight() << endl;
-        cout << "Beast Level: " << bear->getBeastLevel() << endl;
-        cout << "What it eats: " << bear->getWhatItEats() << endl;
-        cout << "Name: " << bear->getName() << endl;
-        cout << "Sound: " << bear->getSound() << endl;
-        cout << "Color: " << bear->getColor() << endl;
-        cout << "Type: " << bear->getType() << endl;
+        // Create a QString to hold the details of the bear.
+        QString bearDetails;
+        bearDetails.append("<b>Bear details:</b><br>");
+        bearDetails.append("Weight: " + QString::number(bear->getWeight()) + "<br>");
+        bearDetails.append("Height: " + QString::number(bear->getHeight()) + "<br>");
+        bearDetails.append("Beast Level: " + QString::number(bear->getBeastLevel()) + "<br>");
+        bearDetails.append("What it eats: " + QString::fromStdString(bear->getWhatItEats()) + "<br>");
+        bearDetails.append("Name: " + QString::fromStdString(bear->getName()) + "<br>");
+        bearDetails.append("Sound: " + QString::fromStdString(bear->getSound()) + "<br>");
+        bearDetails.append("Color: " + QString::fromStdString(bear->getColor()) + "<br>");
+        bearDetails.append("Type: " + QString::fromStdString(bear->getType()) + "<br><br>");
+
 
         calc->add(1, 2);
         calc->add(1.0, 2.0);
         calc->subtract(1, 2);
         Calculator::subtract(1.0, 2.0);
         calc->multiply(1, 2);
-        cout << "Successfully created an animal and calculator object." << endl;
 
-    } catch (exception &e) {
-        cout << "Error: " << e.what() << endl;
-    }
+        bearDetails.append("Successfully created an animal and calculator object.\n");
 
-    // try to create the QApplication
-    try {
-        QApplication app(argc, argv);
-        QLabel label("Hello Qt!  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
-                     "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation "
-                     "ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in "
-                     "reprehenderit in ""voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur "
-                     "sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est "
-                     "laborum");
+        // Create the QLabel with the bear details
+        QLabel label(bearDetails);
+        label.setTextFormat(Qt::RichText);
+        label.setWindowTitle("Animal Details");
+        label.setMargin(10); // Add some margin for aesthetic spacing
         label.show();
-        return QApplication::exec();
+
+        return app.exec(); // Run the application event loop
+
     } catch (exception &e) {
-        cout << "Error: " << e.what() << endl;
+        qDebug() << "Error:" << e.what();
     }
     return 0;
 }
-
